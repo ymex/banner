a5c0c44a61c1d01ed4adf5ec5f9e5f7d5fb5f707
王乐朋 <ymex@foxmail.com>
2018-01-04 16:28:06 +0800

    add scaleType for createViewcaller

--- banner/src/main/java/cn/ymex/widget/banner/callback/CreateViewCaller.java
+++ banner/src/main/java/cn/ymex/widget/banner/callback/CreateViewCaller.java
@@ -5,14 +5,27 @@ import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import cn.ymex.widget.banner.callback.CreateViewCallBack;
 
 /**
  * 默认加载布局
  */
 public class CreateViewCaller implements CreateViewCallBack<FrameLayout> {
+    private ImageView.ScaleType scaleType;
 
     public static CreateViewCaller build() {
-        return new CreateViewCaller();
+        return new CreateViewCaller(ImageView.ScaleType.FIT_XY);
+    }
+
+    public static CreateViewCaller build(ImageView.ScaleType scaleType) {
+        return new CreateViewCaller(scaleType);
+    }
+
+
+    public CreateViewCaller(ImageView.ScaleType scaleType) {
+        this.scaleType = scaleType;
     }
 
     public static AppCompatImageView findImageView(View view) {
@@ -37,7 +50,7 @@ public class CreateViewCaller implements CreateViewCallBack<FrameLayout> {
 
         AppCompatImageView view = new AppCompatImageView(context);
         view.setLayoutParams(params);
-        view.setScaleType(AppCompatImageView.ScaleType.FIT_XY);
+        view.setScaleType(scaleType);
 
         layout.addView(view, params);
         return layout;
